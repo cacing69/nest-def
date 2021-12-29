869f198ee70a22ec79aef88745bbdca4d81afe8f
cacing69 <cacingworm69@gmail.com>
2021-12-29 21:55:32 +0700

    update route auth/token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { Public } from 'src/decorators/public.decorator';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { LocalAuthGuard } from './local-auth.guard';
@@ -6,20 +7,16 @@ import { LocalAuthGuard } from './local-auth.guard';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
-  @Get()
-  getAuth(): object {
-    return {
-      route: 'auth',
-    };
-  }
 
+  @Public()
   @UseGuards(LocalAuthGuard)
-  @Post('login')
+  @Post('token')
   async postToken(@Request() req) {
     return this.authService.login(req.user);
     //   return req.user;
   }
 
+  // curl http://localhost:3000/auth/profile -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImRldmVsb3BlciIsInN1YiI6MSwiaWF0IjoxNjQwNzg5NjkyLCJleHAiOjE2NDEzOTQ0OTJ9.WQ_Tb-4GfhYumUILcpErNroodAYdNHFQVeNU5UeNNuw"
   @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
