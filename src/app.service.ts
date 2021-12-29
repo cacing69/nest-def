00f5f74da2cd13a415b5db0b7c2c7f8c02fe2a18
cacing69 <cacingworm69@gmail.com>
2021-12-29 15:56:25 +0700

    a

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,7 +6,7 @@ export class AppService {
     return {
       app: 'api',
       version: '1.0.1',
-      env: process.env.ENV_CHECK,
+      env: process.env.NEST_ENV_CHECK,
       creator: ['@cacing69', '@user2'],
     };
   }
