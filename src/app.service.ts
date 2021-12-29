b42028b2fc3895f1756e557180fc0871ad2f4f28
cacing69 <cacingworm69@gmail.com>
2021-12-29 15:44:56 +0700

    add creator

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,11 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHome(): any {
-    return { app: 'api', version: '1.0.1', env: process.env.ENV_CHECK };
+    return {
+      app: 'api',
+      version: '1.0.1',
+      env: process.env.ENV_CHECK,
+      creator: ['@cacing69'],
+    };
   }
 }
