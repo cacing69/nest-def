985f428082d419dd2616c285cadb69e335f5bde7
cacing69 <cacingworm69@gmail.com>
2021-12-29 15:42:06 +0700

    test env

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHome(): any {
-    return { app: 'api', version: '1.0.1' };
+    return { app: 'api', version: '1.0.1', env: process.env.ENV_CHECK };
   }
 }
